8201fed1299c9eed79f143b38ed7269a0f2854bb
yang <yangzq19931003@outlook.com>
2020-07-29 14:45:32 +0800

    rename Class and Test PointActivity to BasicPointRules

--- src/main/java/com.thoughtworks/BasicPointRules.java
+++ src/main/java/com.thoughtworks/BasicPointRules.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
-public class PointActivity {
+public class BasicPointRules {
     private static final BigDecimal BASIC_POINT_EXCHANGE_RATIO = BigDecimal.valueOf(1);
     private static final BigDecimal BENEFIT_POINT_EXCHANGE_RATIO = BigDecimal.valueOf(2);
     private List<GoodsName> basicTaxFreeGoodsTypes = Arrays.asList(GoodsName.APPLE, GoodsName.WATERMELON, GoodsName.REFRIGERATOR);
--- src/test/java/com/thoughtworks/BasicPointRulesTest.java
+++ src/test/java/com/thoughtworks/BasicPointRulesTest.java
@@ -7,23 +7,23 @@ import java.math.BigDecimal;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class PointActivityTest {
+public class BasicPointRulesTest {
     @Test
     public void should_return_true_when_use_function_isPromotional_given_apple(){
-        PointActivity pointActivity = new PointActivity();
+        BasicPointRules basicRules = new BasicPointRules();
         Good apple = new Good(GoodsName.APPLE, BigDecimal.valueOf(100));
 
-        boolean isTrue =  pointActivity.isPromotional(apple);
+        boolean isTrue =  basicRules.isPromotional(apple);
 
         assertTrue(isTrue);
     }
 
     @Test
     public void should_return_points_when_use_function_exchangePoint_given_refrigerator(){
-        PointActivity pointActivity = new PointActivity();
+        BasicPointRules basicRules = new BasicPointRules();
         Good refrigerator = new Good(GoodsName.REFRIGERATOR, BigDecimal.valueOf(700));
 
-        int points =  pointActivity.exchangePoint(refrigerator);
+        int points =  basicRules.exchangePoint(refrigerator);
 
         assertEquals(1400,points);
     }
