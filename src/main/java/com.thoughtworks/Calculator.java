5eca4a5932a6f5ce60acde7a685bcf73c3d67a2f
yang <yangzq19931003@outlook.com>
2020-07-30 10:09:25 +0800

    clean code

--- src/main/java/com.thoughtworks/Calculator.java
+++ src/main/java/com.thoughtworks/Calculator.java
@@ -19,18 +19,27 @@ public class Calculator {
     public int calculatorPointsIsBeyond(ArrayList<Good> goods, BeyondPointRules rules){
         int points;
         BigDecimal benefitGoodsMoney = rules.getBenefitGoodsMoney(goods);
+        BigDecimal totalMoney = rules.getGoodsMoney(goods);
         BigDecimal moneyBoundary = BeyondPointRules.getMoneyBoundary();
         BigDecimal basicRatio = BeyondPointRules.getBasicPointExchangeRatio();
-        BigDecimal benefitPRatio = BeyondPointRules.getBenefitPointExchangeRatio();
+        BigDecimal benefitRatio = BeyondPointRules.getBenefitPointExchangeRatio();
         BigDecimal beyondRatio = BeyondPointRules.getBeyondBoundaryExchangeRatio();
 
-        if(benefitGoodsMoney.compareTo(BeyondPointRules.getMoneyBoundary())>0) {
-            points = moneyBoundary.intValue() * benefitPRatio.intValue();
-            points += rules.getGoodsMoney(goods).subtract(benefitGoodsMoney).multiply(beyondRatio).setScale(0,BigDecimal.ROUND_DOWN).intValue();
+        if(benefitGoodsMoney.compareTo(moneyBoundary)>0) {
+            points = moneyBoundary.multiply(benefitRatio).intValue();
+            points += totalMoney.subtract(benefitGoodsMoney)
+                                .multiply(beyondRatio)
+                                .setScale(0,BigDecimal.ROUND_DOWN)
+                                .intValue();
         }else{
-            points = benefitGoodsMoney.intValue() * benefitPRatio.intValue();
-            points += (moneyBoundary.subtract(benefitGoodsMoney)).intValue() * basicRatio.intValue();
-            points += rules.getGoodsMoney(goods).subtract(moneyBoundary).multiply(beyondRatio).setScale(0,BigDecimal.ROUND_DOWN).intValue();
+            points = benefitGoodsMoney.multiply(benefitRatio).intValue();
+            points += moneyBoundary.subtract(benefitGoodsMoney)
+                                   .multiply(basicRatio)
+                                   .intValue();
+            points += totalMoney.subtract(moneyBoundary)
+                                .multiply(beyondRatio)
+                                .setScale(0,BigDecimal.ROUND_DOWN)
+                                .intValue();
         }
         return points;
     }
